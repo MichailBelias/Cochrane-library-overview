f31232771fc8e5e85ad7ebf7b727821613f0ba42
Fideborios <touringlxs@gmail.com>
2019-05-06 00:18:59 +0200

    fhdf

--- IPD-MA Cochrane papers/Abstract text mining/Abtract Text Mining.R
+++ IPD-MA Cochrane papers/Abstract text mining/Abtract Text Mining.R
@@ -13,14 +13,8 @@ library(xlsx)
 library(parallel)
 library(foreach)
 library(doParallel)
+library(readxl)
 
-
-
-# First we download the abstract
-IPD_MA = read.xlsx(file = "IPD-MA Cochrane papers/IPD-MAs in General.xlsx", sheetName = "Pub med IPD-MA articles", as.data.frame = T)
-IPD_MA[IPD_MA == "NA"] <- NA
-write.xlsx(IPD_MA , "IPD-MA Cochrane papers/temp.xlsx")
-# Query pubmed and fetch many results
 
 
 
@@ -28,6 +22,7 @@ keywords =  c("assess", "examine", "evaluate", "establish" ," provide", "investi
 
 Keyword_in_Abs = list()
 Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 4137, ncol = 1))
+colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
 
 for ( i in 1:4137){
   temp = vector()
@@ -44,7 +39,6 @@ for ( i in 1:4137){
   if(is.na(IPD_MA[i,]$Goal)){
     IPD_MA[i,]$Goal = Keyword_in_Abs_DF[i,]
   }
-  
 }
 
 
@@ -79,8 +73,48 @@ for ( i in 1:4137){
 table(is.na(IPD_MA$`Type of primary outcome`))
 
 
+# First we download the abstract
+IPD_MA <- read_excel("IPD-MA Cochrane papers/IPD-MAs in General.xlsx", sheet =  "Pub med IPD-MA articles")
+IPD_MA[IPD_MA == "NA"] <- NA
+# write.xlsx(IPD_MA , "IPD-MA Cochrane papers/temp.xlsx")
+# Query pubmed and fetch many results
+
+Abstracts =  (strsplit(as.character(IPD_MA$Abstract), split =  "(?<=[[:punct:]])\\s(?=[A-Z])",  perl = T))
+
+IPD_MA$Goal =  as.character(IPD_MA$Goal)
+IPD_MA$`Type of primary outcome` =  as.character(IPD_MA$`Type of primary outcome`)
+IPD_MA$`Type of studies
+ included` =  as.character(IPD_MA$`Type of studies
+ included`)
+
+
+keywords =   c("randomised", "RCT" ,"RCTs")
+
+Keyword_in_Abs = list()
+Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 4137, ncol = 1))
+
+for ( i in 1:4137){
+  temp = vector()
+  for ( j in 1: length( Abstracts[[i]])){
+    temp[j] = any(keywords %in% unlist(strsplit(Abstracts[[i]][j], split =  " ") ))
+  }
+  
+  if (any(temp)){
+    Keyword_in_Abs_DF[i,] = paste(Abstracts[[i]][which(temp)], collapse = " ")
+  }else{
+    Keyword_in_Abs_DF[i,] = NA
+  }
+  
+  if(is.na(IPD_MA[i,]$`Type of primary outcome`)){
+    IPD_MA[i,]$`Type of studies
+ included` = Keyword_in_Abs_DF[i,]
+  }
+  
+}
+
+table(is.na(Keyword_in_Abs_DF))
 
 
 
-
+write.xlsx(IPD_MA , "IPD-MA Cochrane papers/temp.xlsx")
 
