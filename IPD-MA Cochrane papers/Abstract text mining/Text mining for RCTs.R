0e6d03ecde75e1ada1a66f777bfa04bd5e003219
Fideborios <touringlxs@gmail.com>
2019-05-20 13:47:37 +0200

    OK

--- IPD-MA Cochrane papers/Abstract text mining/Text mining for RCTs.R
+++ IPD-MA Cochrane papers/Abstract text mining/Text mining for RCTs.R
@@ -21,12 +21,35 @@ Abstracts = list()
 
 for(i in 1:4137){
   
-  Abstracts[[i]] = unlist(strsplit(IPD_MA[i,]$Abstract, "(?<=[[:punct:]])\\s(?=[A-Z])", perl=T))
+  Abstracts[[i]] = unlist(strsplit(IPD_MA[i,]$Abstract, "(?<=\\.)\\s(?=[A-Z])", perl=T))
   
 }
 
-
 ### Find phrases such as randomised clinical trials , cohort studies, etc
+check =  grep("randomised clinical | randomised controlled | RCT | randomised control | placebo | randomized clinical | randomized controlled | RCT | randomized control | placebo ", lapply(Abstracts, tolower))
+
+for ( i in check){
+temp = vector()
+temp = grep("randomised clinical | randomised controlled | RCT | randomised control | placebo | randomized clinical | randomized controlled | RCT | randomized control | placebo ", 
+            lapply(Abstracts[[i]], tolower),value = F)
+print(temp)
+  
+if (!is.na(any(temp))){
+    Keyword_in_Abs_DF[i,] = paste(Abstracts[[i]][temp], collapse = " ")
+  }else{
+    Keyword_in_Abs_DF[i,] = NA
+  }
+  
+  if(is.na(IPD_MA[i,]$`Type of studies included`)){
+    IPD_MA[i,]$`Type of studies included` = Keyword_in_Abs_DF[i,]
+    print("Extra")
+  }
+}
+
+
+write.xlsx(IPD_MA , "IPD-MA Cochrane papers/temp.xlsx", showNA = F)
+
+
 
 
 
@@ -39,28 +62,6 @@ grep("mixed-effects", lapply(Abstracts, tolower))
 Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 4137, ncol = 1)); colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
 
 check =  grep("randomised clinical trials | randomised controlled | rct  | cohort studies | population-based studies", lapply(Abstracts, tolower))
-
-for ( i in check){
-
-temp = vector()
-temp = grep("randomised clinical trials | randomised controlled | rct  | cohort studies | population-based studies", 
-            lapply(Abstracts[[i]], tolower),value = F)
-print(temp)
-  
-if (!is.na(any(temp))){
-    Keyword_in_Abs_DF[i,] = paste(Abstracts[[i]][temp], collapse = " ")
-  }else{
-    Keyword_in_Abs_DF[i,] = NA
-  }
-  
-  if(is.na(IPD_MA[i,]$`Type of studies
- included`)){
-    IPD_MA[i,]$`Type of studies
- included` = Keyword_in_Abs_DF[i,]
-  }
-}
-
-write.xlsx(IPD_MA , "IPD-MA Cochrane papers/temp.xlsx", showNA = F)
 
 
 
