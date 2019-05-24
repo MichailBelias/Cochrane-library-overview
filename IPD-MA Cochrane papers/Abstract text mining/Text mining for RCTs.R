d64a9365a7f3dd9262e166bd98fdf8ad26acb610
Fideborios <touringlxs@gmail.com>
2019-05-24 17:13:40 +0200

    What the

--- IPD-MA Cochrane papers/Abstract text mining/Text mining for RCTs.R
+++ IPD-MA Cochrane papers/Abstract text mining/Text mining for RCTs.R
@@ -1,8 +1,5 @@
 #### Full text 
 ## with this 
-
-
-
 library(easyPubMed)
 library(dplyr)
 library(kableExtra)
@@ -12,25 +9,30 @@ library(parallel)
 library(foreach)
 library(doParallel)
 
+IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/6. Data/IPD_test.xlsx", sheet= 1)
 
-IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/IPD-MAs in General.xlsx", sheet = "Pub med IPD-MA articles")
+
+#IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/IPD-MAs in General.xlsx", sheet = "Pub med IPD-MA articles")
 IPD_MA[IPD_MA == "NA"] <- NA
 # Get Abstract in a list
 Abstracts = list()
 
 
-for(i in 1:4137){
+for(i in 1:1538){
   
   Abstracts[[i]] = unlist(strsplit(IPD_MA[i,]$Abstract, "(?<=\\.)\\s(?=[A-Z])", perl=T))
   
 }
 
 ### Find phrases such as randomised clinical trials , cohort studies, etc
-check =  grep("randomised clinical | randomised controlled | RCT | randomised control | placebo | randomized clinical | randomized controlled | RCT | randomized control | placebo ", lapply(Abstracts, tolower))
+check =  grep("randomised clinical | randomised controlled | prospective | case-controls | case-reports | RCT | randomised control | placebo | cohort studies | observational studies| randomized clinical | randomized controlled | RCT | randomized control | placebo ",
+              lapply(Abstracts, tolower))
+
+Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 1538, ncol = 1)); colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
 
 for ( i in check){
 temp = vector()
-temp = grep("randomised clinical | randomised controlled | RCT | randomised control | placebo | randomized clinical | randomized controlled | RCT | randomized control | placebo ", 
+temp = grep("randomised clinical | randomised controlled | prospective | case-controls | case-reports | RCT | randomised control | placebo | cohort studies | observational studies| randomized clinical | randomized controlled | RCT | randomized control | placebo ",
             lapply(Abstracts[[i]], tolower),value = F)
 print(temp)
   
@@ -46,34 +48,16 @@ if (!is.na(any(temp))){
   }
 }
 
+table(is.na(Keyword_in_Abs_DF))
 
-write.xlsx(IPD_MA , "IPD-MA Cochrane papers/temp.xlsx", showNA = F)
+keywords = c("assess", "assessed", "examine","examined", "evaluate", "establish", "explore","purpose", 
+             "provide", "investigate","investigated" , "goal", "understand", "aim",
+             "identify", "determine", "predict", "conduct","conducted", "aimed")
 
-
-
-
-
-grep("randomised clinical trials | randomised controlled | RCT ", lapply(Abstracts, tolower))
-
-
-grep("mixed-effects", lapply(Abstracts, tolower))
-
-
-Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 4137, ncol = 1)); colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
-
-check =  grep("randomised clinical trials | randomised controlled | rct  | cohort studies | population-based studies", lapply(Abstracts, tolower))
-
-
-
-keywords = c("assess", "examine", "evaluate", "establish", "explore",
-             "provide", "investigate" , "goal", "understand", "aim",
-             "identify", "determine", "predict", "conduct")
-
-Keyword_in_Abs = list()
-Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 4137, ncol = 1))
+Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 1538, ncol = 1))
 colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
 
-for ( i in 50:4137){
+for ( i in 1:1538){
   temp = vector()
   for ( j in 1: length( Abstracts[[i]])){
     temp[j] = any(keywords %in% unlist(strsplit(Abstracts[[i]][j], split =  " ") ))
@@ -91,6 +75,52 @@ for ( i in 50:4137){
 }
 
 table(is.na(Keyword_in_Abs_DF))
+
+
+
+
+
+write.xlsx(IPD_MA , "IPD-MA Cochrane papers/temp.xlsx", showNA = F)
+
+
+### Find phrases cohort studies, etc
+check =  grep("cohort", lapply(Abstracts, tolower))
+
+Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 4137, ncol = 1)); colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
+
+for ( i in check){
+  temp = vector()
+  temp = grep("randomised clinical | randomised controlled | RCT | randomised control | placebo | 
+            randomized clinical | randomized controlled | RCT | randomized control | placebo ", 
+              lapply(Abstracts[[i]], tolower),value = F)
+  print(temp)
+  
+  if (!is.na(any(temp))){
+    Keyword_in_Abs_DF[i,] = paste(Abstracts[[i]][temp], collapse = " ")
+  }else{
+    Keyword_in_Abs_DF[i,] = NA
+  }
+  
+  if(is.na(IPD_MA[i,]$`Type of studies included`)){
+    IPD_MA[i,]$`Type of studies included` = Keyword_in_Abs_DF[i,]
+    print("Extra")
+  }
+}
+
+
+
+
+
+grep("randomised clinical trials | randomised controlled | RCT ", lapply(Abstracts, tolower))
+
+
+grep("mixed-effects", lapply(Abstracts, tolower))
+
+
+Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 4137, ncol = 1)); colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
+
+check =  grep("randomised clinical trials | randomised controlled | rct  | cohort studies | population-based studies", lapply(Abstracts, tolower))
+
 
 write.xlsx(IPD_MA , "IPD-MA Cochrane papers/temp.xlsx", showNA = F)
 
