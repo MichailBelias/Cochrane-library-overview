f2869fcd20bab301aea28010e1c1d6887274b9ab
Fideborios <touringlxs@gmail.com>
2019-05-29 21:28:17 +0200

    OK

--- IPD-MA Cochrane papers/Abstract text mining/Text mining for RCTs.R
+++ IPD-MA Cochrane papers/Abstract text mining/Text mining for RCTs.R
@@ -51,7 +51,7 @@ if (!is.na(any(temp))){
 table(is.na(Keyword_in_Abs_DF))
 
 keywords = c("assess", "assessed", "examine","examined", "evaluate", "establish", "explore","purpose", 
-             "provide", "investigate","investigated" , "goal", "understand", "aim",
+             "provide", "investigate","investigated" , "goal", "understand", "aim", "aimed",
              "identify", "determine", "predict", "conduct","conducted", "aimed")
 
 Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 1538, ncol = 1))
@@ -77,10 +77,21 @@ for ( i in 1:1538){
 table(is.na(Keyword_in_Abs_DF))
 
 
-write.xlsx(IPD_MA , "IPD-MA Cochrane papers/6. Data/IPD_test.xlsx", showNA = F)
+IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/6. Data/New search in Pubmed.xlsx", sheet= 1)
 
 
-### Find phrases cohort studies, etc
+#IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/IPD-MAs in General.xlsx", sheet = "Pub med IPD-MA articles")
+IPD_MA[IPD_MA == "NA"] <- NA
+# Get Abstract in a list
+Abstracts = list()
+
+for(i in 1:1538){
+  
+  Abstracts[[i]] = unlist(strsplit(IPD_MA[i,]$Abstract, "(?<=\\.)\\s(?=[A-Z])", perl=T))
+  
+}
+
+### Find phrases diagnostic studies, etc
 check =  grep("diagnostic", lapply(Abstracts, tolower))
 
 Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 4137, ncol = 1)); colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
@@ -97,34 +108,57 @@ for ( i in check){
     Keyword_in_Abs_DF[i,] = NA
   }
   
+  if(is.na(IPD_MA[i,]$`IPD meta-analysis Review Guidelines Methodological`)){
+    IPD_MA[i,]$`IPD meta-analysis Review Guidelines Methodological`= Keyword_in_Abs_DF[i,]
+    print("Extra")
+  }
+}
+
+
+IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/6. Data/New search in Pubmed.xlsx", sheet= 1)
+
+
+#IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/IPD-MAs in General.xlsx", sheet = "Pub med IPD-MA articles")
+IPD_MA[IPD_MA == "NA"] <- NA
+# Get Abstract in a list
+Abstracts = list()
+
+for(i in 1:1538){
+  
+  Abstracts[[i]] = unlist(strsplit(IPD_MA[i,]$Abstract, "(?<=\\.)\\s(?=[A-Z])", perl=T))
+  
+}
+
+### Find phrases "We included xxx trials and xxx participants"
+check =  grep("randomised clinical | randomised controlled | prospective | case-controls | case-reports | RCT | randomised control | placebo | cohort studies | observational studies| randomized clinical | randomized controlled | RCT | randomized control | placebo ",
+              lapply(Abstracts, tolower))
+
+Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 1538, ncol = 1)); colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
+
+for ( i in check){
+  temp = vector()
+  temp = grep("randomised clinical | randomised controlled | prospective | case-controls | case-reports | RCT | randomised control | placebo | cohort studies | observational studies| randomized clinical | randomized controlled | RCT | randomized control | placebo ",
+              lapply(Abstracts[[i]], tolower),value = F)
+  print(temp)
+  
+  if (!is.na(any(temp))){
+    Keyword_in_Abs_DF[i,] = paste(Abstracts[[i]][temp], collapse = " ")
+  }else{
+    Keyword_in_Abs_DF[i,] = NA
+  }
+  
   if(is.na(IPD_MA[i,]$`Type of studies included`)){
     IPD_MA[i,]$`Type of studies included` = Keyword_in_Abs_DF[i,]
     print("Extra")
   }
 }
 
+# Query pubmed and fetch many results
+c(paste(IPD_MA$EntrezUID[1], "[uid]",sep = ""),
+              paste(" OR ",IPD_MA$EntrezUID[2:300], "[uid]",sep = ""))
+
+
+write.xlsx(IPD_MA , "IPD-MA Cochrane papers/6. Data/IPD_test.xlsx", showNA = F)
 
 
 
-
-grep("randomised clinical trials | randomised controlled | RCT ", lapply(Abstracts, tolower))
-
-
-grep("mixed-effects", lapply(Abstracts, tolower))
-
-
-Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 4137, ncol = 1)); colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
-
-check =  grep("randomised clinical trials | randomised controlled | rct  | cohort studies | population-based studies", lapply(Abstracts, tolower))
-
-
-write.xlsx(IPD_MA , "IPD-MA Cochrane papers/temp.xlsx", showNA = F)
-
-temp = IPD_MA
-
-
-IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/IPD-MAs in General.xlsx", sheet = "Pub med IPD-MA articles")
-IPD_MA[IPD_MA == "NA"] <- NA
-
-table(is.na(temp$Goal))
-table(is.na(IPD_MA$Goal))
