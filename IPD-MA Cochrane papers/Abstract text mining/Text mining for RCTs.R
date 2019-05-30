476aa74c0232371ec0d51a37a5e81632efc555d4
Fideborios <touringlxs@gmail.com>
2019-05-30 23:31:59 +0200

    ok

--- IPD-MA Cochrane papers/Abstract text mining/Text mining for RCTs.R
+++ IPD-MA Cochrane papers/Abstract text mining/Text mining for RCTs.R
@@ -15,6 +15,10 @@ IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/6. Data/New search in Pubmed.
 #IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/IPD-MAs in General.xlsx", sheet = "Pub med IPD-MA articles")
 IPD_MA[IPD_MA == "NA"] <- NA
 # Get Abstract in a list
+
+
+table(IPD_MA$`Type of studies included`)
+
 Abstracts = list()
 
 
@@ -25,8 +29,7 @@ for(i in 1:1538){
 }
 
 ### Find phrases such as randomised clinical trials , cohort studies, etc
-check =  grep("randomised clinical | randomised controlled | prospective | case-controls | case-reports | RCT | randomised control | placebo | cohort studies | observational studies| randomized clinical | randomized controlled | RCT | randomized control | placebo ",
-              lapply(Abstracts, tolower))
+check =  grep("randomised clinical | randomised controlled | prospective | case-controls | case-reports | RCT | randomised control | placebo | genome-wide | cohort studies | observational studies| randomized clinical | randomized controlled | RCT | randomized control | prospective studies ",lapply(Abstracts, tolower))
 
 Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 1538, ncol = 1)); colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
 
@@ -48,7 +51,49 @@ if (!is.na(any(temp))){
   }
 }
 
-table(is.na(Keyword_in_Abs_DF))
+table(is.na(IPD_MA$`Type of studies included`))
+
+library(xlsx)
+write.xlsx2(IPD_MA,"IPD-MA Cochrane papers/6. Data/IPD_test.xlsx")
+
+
+#### This search query is for the Type of Outcomes
+IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/6. Data/New search in Pubmed.xlsx", sheet= 1)
+
+
+#IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/IPD-MAs in General.xlsx", sheet = "Pub med IPD-MA articles")
+IPD_MA[IPD_MA == "NA"] <- NA
+# Get Abstract in a list
+
+keywords = c("hazard")
+
+Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 1538, ncol = 1))
+colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
+
+for ( i in 1:1538){
+  
+  temp = vector()
+  for ( j in 1: length( Abstracts[[i]])){
+    temp[j] = any(keywords %in% unlist(strsplit(tolower(Abstracts[[i]][j]), split =  " ") ))
+  }
+  
+  if (any(temp)){
+    Keyword_in_Abs_DF[i,] = paste(Abstracts[[i]][which(temp)], collapse = " ")
+  }else{
+    Keyword_in_Abs_DF[i,] = NA
+  }
+  
+  if(is.na(IPD_MA[i,]$`Effect estimate`)){
+    IPD_MA[i,]$`Effect estimate` = Keyword_in_Abs_DF[i,]
+  }
+}
+
+table(is.na(IPD_MA$`Effect estimate`))
+
+
+write.xlsx2(IPD_MA,"IPD-MA Cochrane papers/6. Data/IPD_test.xlsx")
+
+
 
 keywords = c("assess", "assessed", "examine","examined", "evaluate", "establish", "explore","purpose", 
              "provide", "investigate","investigated" , "goal", "understand", "aim", "aimed",
@@ -74,7 +119,7 @@ for ( i in 1:1538){
   }
 }
 
-table(is.na(Keyword_in_Abs_DF))
+table(is.na(IPD_MA$`Type of studies included`))
 
 
 IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/6. Data/New search in Pubmed.xlsx", sheet= 1)
@@ -90,6 +135,9 @@ for(i in 1:1538){
   Abstracts[[i]] = unlist(strsplit(IPD_MA[i,]$Abstract, "(?<=\\.)\\s(?=[A-Z])", perl=T))
   
 }
+
+
+
 
 ### Find phrases diagnostic studies, etc
 check =  grep("diagnostic", lapply(Abstracts, tolower))
@@ -115,9 +163,40 @@ for ( i in check){
 }
 
 
+### Find phrases Risk ratios and relative risks studies, etc
 IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/6. Data/New search in Pubmed.xlsx", sheet= 1)
+check =  grep("risk ratio | relative risk | relative-risk | risk-ratio", lapply(Abstracts, tolower))
+
+Keyword_in_Abs_DF = data.frame(matrix(NA, nrow = 1492, ncol = 1)); colnames(Keyword_in_Abs_DF) = "Search in Abstracts"
+
+for ( i in check){
+  temp = vector()
+  temp = grep("risk ratio | relative risk | relative-risk | risk-ratio", lapply(Abstracts[[i]], tolower),value = F)
+  print(temp)
+  
+  if (!is.na(any(temp))){
+    Keyword_in_Abs_DF[i,] = paste(Abstracts[[i]][temp], collapse = " ")
+  }else{
+    Keyword_in_Abs_DF[i,] = NA
+  }
+  
+  if(!is.na(IPD_MA[i,]$`Effect estimate`)){
+    IPD_MA[i,]$`Effect estimate`= paste(IPD_MA[i,]$`Effect estimate` ,  Keyword_in_Abs_DF[i,] ,  sep = " ; ")
+    print(paste(i, "Extra"))
+  }
+}
 
 
+write.xlsx2(IPD_MA,"IPD-MA Cochrane papers/6. Data/IPD_test.xlsx")
+
+
+
+
+
+
+
+
+IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/6. Data/New search in Pubmed.xlsx", sheet= 1)
 #IPD_MA = readxl::read_xlsx("IPD-MA Cochrane papers/IPD-MAs in General.xlsx", sheet = "Pub med IPD-MA articles")
 IPD_MA[IPD_MA == "NA"] <- NA
 # Get Abstract in a list
@@ -152,11 +231,6 @@ for ( i in check){
     print("Extra")
   }
 }
-
-# Query pubmed and fetch many results
-c(paste(IPD_MA$EntrezUID[1], "[uid]",sep = ""),
-              paste(" OR ",IPD_MA$EntrezUID[2:300], "[uid]",sep = ""))
-
 
 write.xlsx(IPD_MA , "IPD-MA Cochrane papers/6. Data/IPD_test.xlsx", showNA = F)
 
